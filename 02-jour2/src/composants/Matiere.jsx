cec142120f8208e794b221ad10c411eab6221209
Malik <malik.h@webdevpro.net>
2022-12-19 16:06:03 +0100

    correction suite

--- 02-jour2/src/composants/Matiere.jsx
+++ 02-jour2/src/composants/Matiere.jsx
@@ -8,12 +8,21 @@ const Matiere = (props) => {
  
 export default Matiere; */
 
-
-// solution 2
+/* 
+solution 2
 const Matiere = ({matiere}) => {
     return ( <p>
         nom : {matiere.nom} - duree {matiere.duree} {matiere.unite}
     </p> );
 }
  
-export default Matiere; 
+export default Matiere; */
+
+const Matiere = ({matiere}) => {
+    const {nom , duree, unite } = matiere ;
+    return ( <p>
+        nom : {nom} - duree {duree} {unite}
+    </p> );
+}
+ 
+export default Matiere;
