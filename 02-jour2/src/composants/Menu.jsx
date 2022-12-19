b2907a02bd7f8121e5cad474a9eb9d2872a3606e
Malik <malik.h@webdevpro.net>
2022-12-19 09:59:39 +0100

    création d'un menu

---
+++ 02-jour2/src/composants/Menu.jsx
@@ -0,0 +1,22 @@
+/* const Menu = () => {
+
+}
+
+export default Menu ; */
+
+// snippet => quelques lettres qui génère un block de code
+// sfc
+const Menu = () => {
+    return ( 
+        <div className="bg-primary">
+            <nav className="navbar navbar-expand navbar-light container mb-3">
+                <span className="navbar-brand">
+                    Jour2
+                </span>
+            </nav>
+        </div>
+    );
+}
+ 
+export default Menu;    
+
