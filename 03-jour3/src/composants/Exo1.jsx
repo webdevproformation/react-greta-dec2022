1277c5cbce60f53f678e46d09d000d5debb3e2aa
Malik <malik.h@webdevpro.net>
2022-12-20 15:18:41 +0100

    correction

--- 03-jour3/src/composants/Exo1.jsx
+++ 03-jour3/src/composants/Exo1.jsx
@@ -4,6 +4,12 @@ import { reducer } from "../reducer/likeReducer"
 
 const Exo1 = () => {
     const [like , dispatch] = useReducer(reducer  , 0)
+    // useState => idéal primitive 0 "texte" boolean et local (uniquement dans le composant)
+    // useReducer => idéal []  {}  / utiliser mon état dans PLUSIEURS composants 
+    // hook => state manager 
+    // 
+    // REDUX 
+    // rdv 15h35 bon café @ toute suite !!!!
     return ( <>
         <h2>Exo1</h2>
         <p>{like}</p>
