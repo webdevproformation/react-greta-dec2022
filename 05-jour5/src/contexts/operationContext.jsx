35409c2df3e89a7d29b0510f8094fad4ed35e13c
Malik <malik.h@webdevpro.net>
2022-12-22 16:56:48 +0100

    localStorage

--- 05-jour5/src/contexts/operationContext.jsx
+++ 05-jour5/src/contexts/operationContext.jsx
@@ -7,7 +7,8 @@ export const OperationContext = createContext() ;
 
 // c 
 export const OperationContextProvider = (props) => {
-    const data = [ // e
+    
+    const data = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data")) : [ // e
         {id : 1 , nom : "billet" , montant : -500, status : true} ,
         {id : 2 , nom : "salaire" , montant : 700, status : false}    
     ]
@@ -24,27 +25,33 @@ function reducer(state, action){ // d
     switch(action.type){
         case "ADD" : // #16
             const operation = {
-                id : Math.random() + "",
+                //id : Math.random() + "",
+                id : cloneState[cloneState.length -1].id  + 1,
                 nom : action.payload.nom ,
                 montant : action.payload.montant,
                 status : false
             }
             cloneState.push(operation)
+            localStorage.setItem("data" , JSON.stringify(cloneState))
             return cloneState; 
         case "SUPPR" : // 17
-            return cloneState.filter( function (item) {
+            const modif = cloneState.filter( function (item) {
                 return item.id !== action.payload
             } )
+            localStorage.setItem("data" , JSON.stringify(modif))
+            return modif
         case "MODIF" : // 18
             return cloneState.map( function (item) {
                 if(item.id === action.payload) return {...item , status : !item.status}
                 return item
             } )
         case "VALIDER" : // 19
-            return cloneState.map( function (item) {
+            const modif3 = cloneState.map( function (item) {
                 if(item.id === action.payload.id) return { ...action.payload , montant: parseInt(action.payload.montant),  status : !item.status}
                 return item
             } )
+            localStorage.setItem("data" , JSON.stringify(modif3))
+            return modif3 
         default :
             return state 
     }
