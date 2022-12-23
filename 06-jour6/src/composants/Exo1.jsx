cab02872ab63e66d3c13d5120a6f32d3c8485ad1
Malik <malik.h@webdevpro.net>
2022-12-23 16:10:49 +0100

    enoncé

--- 06-jour6/src/composants/Exo1.jsx
+++ 06-jour6/src/composants/Exo1.jsx
@@ -4,7 +4,6 @@ const Exo1 = () => {
 
     const users = useExo1(); // useState / useEffect / requête ajax => stocker dans une fonction à part 
     // custom hook 
-
     return ( <>
         <h2>Exo 1</h2>
         <div className="row">
@@ -20,3 +19,14 @@ const Exo1 = () => {
 }
  
 export default Exo1;
+
+// créer le composant Cocktails 
+// appeler l'api suivante 
+// https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+// réaliser une requête sur cette API 
+// affichage 
+/* nom 
+img 
+instruction de préparation
+=> essayer de mettre l'ensemble des hooks utilisé par ce composant  dans un hook personnalisé 
+useCocktails */
