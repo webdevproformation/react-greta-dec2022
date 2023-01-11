458c09219572d1701b6fbd817439dd11082e31d0
Malik <malik.h@webdevpro.net>
2023-01-11 09:57:21 +0100

    barre de menu

---
+++ blog/src/composants/Nav.jsx
@@ -0,0 +1,21 @@
+const Nav = () => {
+    return ( 
+        <div className="bg-danger">
+            <nav className="navbar navbar-expand container px-3 navbar-light">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Accueil</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Créer profil</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Nav;
