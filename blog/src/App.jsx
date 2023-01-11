b2a6218124ee8f7e673a2f999575fe7fe90c2927
Malik <malik.h@webdevpro.net>
2023-01-11 10:37:19 +0100

    créer le router / ajouter outlet / logo

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,11 @@
 import Nav from "./composants/Nav";
+import { Outlet } from "react-router-dom"
 
 const App = () => {
   return ( <>
     <Nav />
     <div className="container">
-      
+      <Outlet /> {/** rappel le système de routing d'Angular */}
     </div>
   </> );
 }
--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter , Routes , Route } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<>coucou les amis</>}  />
+          <Route path="creer-profil" element={<>je crée un profil</>}  />
+          <Route path="connexion" element={<></>}  />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
+
+// test.com
+// test.com/contact
+// test.com/article/1
+// test.com/article/2
+// test.com
+// javascript => https://developer.mozilla.org/en-US/docs/Web/API/History_API
