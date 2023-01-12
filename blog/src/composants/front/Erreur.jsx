5bedbb3073a81dc7dc783a399ed48ca2761c6c92
Malik <malik.h@webdevpro.net>
2023-01-12 11:29:52 +0100

    correction

--- blog/src/composants/front/Erreur.jsx
+++ blog/src/composants/front/Erreur.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
+import {useSelector, useDispatch} from "react-redux"
+
 
 const Erreur = () => {
 
-    const [tableau , setTableau] = useState([1,2,3])
+    // const [tableau , setTableau] = useState([1,2,3])
+    const tableau = useSelector((store) => store.reducerArticle)
+    const setTableau = useDispatch();
 
     return ( <>
         <h1>Erreur</h1>
-        <button onClick={() => setTableau(function(prevTableau){
-            const clonePrev = [...prevTableau]
-            clonePrev.push(4)
-            return clonePrev
-        })}>ajouter</button>
-        {tableau.map(function(chiffre, index){
-            return <p key={index}>{chiffre}</p>
+        <button onClick={() => setTableau({type : "ARTICLE_ADD" , payload : 2 })}>ajouter</button>
+        {tableau.map(function(article, index){
+            return <pre key={index}>
+                {JSON.stringify(article)}
+            </pre>
         })}
     </> );
 }
